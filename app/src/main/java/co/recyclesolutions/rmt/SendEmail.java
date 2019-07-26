d693392a0883ddfc07a440178d77d1e360980e75
AJFNC <ajfnc@hotmail.com>
2019-07-26 00:52:46 -0300

    07/26/2019: The app sends the proposal to the Recycle group via WhatsApp. The client can attach a picture using the camera.

--- app/src/main/java/co/recyclesolutions/rmt/SendEmail.java
+++ app/src/main/java/co/recyclesolutions/rmt/SendEmail.java
@@ -15,11 +15,29 @@ class SendEmail {
     public void sendEmail(Activity act, String name, String phone, String transaction, String type, String qty, String price, String address) {
         String[] TO = {"contato@recyclesolutions.co"}; //E-mail address
         String[] CC = {"alexandre.cavalcanti@recyclesolutions.co"};
-        Intent emailIntent = new Intent(Intent.ACTION_SEND);
-        emailIntent.setData(Uri.parse("mailto:"));
-        emailIntent.setType("text/plain");
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+
+        //Uri uri = Uri.parse("smsto:558788583181");
+
+        //Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:558788583181"));
+        //Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        Intent emailIntent = new Intent("android.intent.action.MAIN");
+
+        //emailIntent.setPackage("com.whatsapp");
+
+        //emailIntent.setData(Uri.parse("mailto:"));
+        //emailIntent.setData(Uri.parse("smsto:558788583181"));
+
+       // emailIntent.setDataAndType(Uri.parse("smsto:558788583181"),"text/plain");
+
+
+
+        //emailIntent.setDataAndType(Uri.parse("mailto:"),"text/plain" );
+
+        //emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        //emailIntent.putExtra(Intent.EXTRA_CC, CC);
+
+        emailIntent.putExtra("jid", "55" + "7488135291" + "@s.whatsapp.net");
+
 
         Date dtToday = new Date();
         dtToday.getTime();
@@ -47,7 +65,9 @@ class SendEmail {
 
 
         // Change with subject, if desired
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Proposta à Recycle Solutions - " + phone);
+        //emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Proposta à Recycle Solutions - " + phone);
+
+
         emailIntent.putExtra(Intent.EXTRA_TEXT, "" +
                 "Sras/Srs\n\n" +
                 "Eu " + name + ", endereço " + address + ", whatsapp nº:" + phone + " venho por meio desta solicitar " + transaction + " de  " + qty + " (kg/l/un) de " + type + " a um valor total de R$ " + price +
@@ -55,8 +75,27 @@ class SendEmail {
                 "Atenciosamente,\n\n" + dtToday + "\n\n" +
                 name);
 
+        emailIntent.setAction(Intent.ACTION_SEND);
+
+        emailIntent.setPackage("com.whatsapp");
+
+        emailIntent.setType("text/plain");
+
+
+
         try {
-            act.startActivity(Intent.createChooser(emailIntent, "Enviar email..."));
+
+
+            //act.startActivity(emailIntent); //Mandar por WhatsApp
+
+            //act.startActivity(Intent.createChooser(emailIntent, "Enviar email..."));
+
+            act.startActivity(emailIntent);
+
+            // Tentativa de acesso ao WhatsApp
+
+
+
             //finish();
         } catch (android.content.ActivityNotFoundException ex) {
             Toast.makeText(act,
