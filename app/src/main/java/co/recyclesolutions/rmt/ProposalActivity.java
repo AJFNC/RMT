693317fb3651d6cdc049275c83c46f8ae4834f00
AJFNC <ajfnc@hotmail.com>
2019-08-28 11:59:39 -0300

    08/28/2019: Included the MercadoPago Checkout payment, but it's missing the PreferencedID.

--- app/src/main/java/co/recyclesolutions/rmt/ProposalActivity.java
+++ app/src/main/java/co/recyclesolutions/rmt/ProposalActivity.java
@@ -11,6 +11,7 @@ import android.webkit.WebViewClient;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,6 +24,16 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+
+import com.mercadopago.android.px.core.MercadoPagoCheckout;
+import com.mercadopago.android.px.*;
+import com.mercadopago.android.px.model.Payment;
+import com.mercadopago.android.px.model.exceptions.MercadoPagoError;
+
+
+//import com.mercadopago.core.MercadoPagoCheckout.*;
+
+//import static android.os.Build.VERSION.SDK;
 
 
 public class ProposalActivity extends AppCompatActivity {
@@ -46,6 +57,10 @@ public class ProposalActivity extends AppCompatActivity {
 
 
     //Counter contPers = new Counter();
+
+
+
+
 
 
     @Override
@@ -139,11 +154,29 @@ public class ProposalActivity extends AppCompatActivity {
 
 
                         if (propTerm.equals("Sim")){
+
+                            System.out.println("[PA] Transação: " + transaction);
+
+                            // Manda a proposta para o Firebase
+                            sendProp();
+
+                            // Se a transação for de "Compra" efetue o pagamento primeiro
+                            if (transaction.equals("b")){
+
+                                // Fazer o CHECKOUT no MercadoPago
+
+                                System.out.println("[PA] Pagamento! ");
+
+                                Toast.makeText(ProposalActivity.this, "Aqui será feito o pagamento",
+                                        Toast.LENGTH_SHORT).show();
+
+                                startMercadoPagoCheckout("");
+
+                            }
                             System.out.println("[PA] Enviando e-mail! ");
                             seProp.sendEmail(ProposalActivity.this, propName, telephone, transaction, propType, propQty, propPrice, propAddress);
 
                             //count = contPers.getCounter();
-                            sendProp();
                         }
 
                         finish();
@@ -364,6 +397,43 @@ public class ProposalActivity extends AppCompatActivity {
     }
 
     */
+
+    // Iniciar o Checkout do MercadoPago
+
+    private static final int REQUEST_CODE = 1;
+
+    private void startMercadoPagoCheckout(final String checkoutPreferenceId) {
+        new MercadoPagoCheckout.Builder("ENV_PUBLIC_KEY", checkoutPreferenceId).build().startPayment(ProposalActivity.this, REQUEST_CODE);
+    }
+
+
+    //
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == MercadoPagoCheckout.PAYMENT_RESULT_CODE) {
+                final Payment payment = (Payment) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_PAYMENT_RESULT);
+                ((TextView) findViewById(R.id.textView11)).setText("Resultado del pago: " + payment.getPaymentStatus());
+                //Done!
+            } else if (resultCode == RESULT_CANCELED) {
+                if (data != null && data.getExtras() != null
+                        && data.getExtras().containsKey(MercadoPagoCheckout.EXTRA_ERROR)) {
+                    final MercadoPagoError mercadoPagoError =
+                            (MercadoPagoError) data.getSerializableExtra(MercadoPagoCheckout.EXTRA_ERROR);
+                    ((TextView) findViewById(R.id.textView11)).setText("Error: " +  mercadoPagoError.getMessage());
+                    //Resolve error in checkout
+                } else {
+                    //Resolve canceled checkout
+                }
+            }
+        }
+    }
+
+
+    //
+
+
 
     @Override
     public void onDestroy(){
